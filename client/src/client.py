9e3c75378926e70b8a630d9f5ad8adb7d3230319
Elena <elena@manele.se>
2020-05-03 18:46:10 +0200

    starting with the device

--- client/src/client.py
+++ client/src/client.py
@@ -4,6 +4,46 @@ import random
 import time
 from fake_android import BluetoothLeAdvertiser, BluetoothLeScanner
 
+class Client: 
+    """ This class represents a device which broadcasts Ephemerals ID (EphIDs) to other devices nearby 
+        It includes 3 files: one with the devices Secrete Key (Sk) for the last 14 days
+        the second containing the list of all EphIDs received from other devices
+        the third with the declared infected Sks. 
+        The device receives EphIDs over a simulated Bluetooth connection"""
+    
+    def __init__(self):
+        self.generate_key()
+        self.add_key_to_file()
+        self.generate_ephids()
+        self.broadcast()
+        self.get_ephids()
+        
+    def generate_key(self):
+        self.key = '123'
+    
+    def add_key_to_file(self):
+        with open('Sk.txt', 'a') as file:
+            file.write(self.key)
+            file.write('\n')
+    
+    def generate_ephids(self):
+        pass
+    
+    def broadcast(self):
+        pass
+    
+    def get_ephids(self):
+        pass
+
+
+client = Client()
+
+
+
+
+
+
+"""
 def receive(data):
     print(f'Received: {data}')
 
@@ -20,3 +60,4 @@ scanner = BluetoothLeScanner(receive)
 # Loop forever
 while True:
     time.sleep(1)
+"""
